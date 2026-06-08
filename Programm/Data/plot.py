287e452d2129cd6fb39a0ab592b04eb17a48115b
Jean-Marc Faucon <rfaucon2@gmail.com>
2026-06-08 18:40:59 +0200

    uyegz

--- Programm/Data/plot.py
+++ Programm/Data/plot.py
@@ -22,19 +22,19 @@ def gen_cmp_N_BH(theta):
     N_avg_list  = []
     BH_avg_list = []
     Tries = 5
-    # Generate data
+    # Génère des données à théta constant et N variable pour les deux algorithmes
     for N in N_list:
         print(f"{theta=}, {N=}")
         N_avg = []
         BH_avg = []
-        for i in range(Tries):                
+        # Execute plusieurs simulations pour chaque algorithme et moyenne les temps de calcul
+        for i in range(Tries):
             seed = randint(0, 100000) 
             execute(f"Simulation N {int(N)} {theta} {seed} N_record.csv")
             with open("N_record.csv", "r+") as N_record:
                 N_record_list = np.array(list(csv.reader(N_record)))[:,1]
                 N_avg.append(average(to_float_list(N_record_list)))
                 
-
         for i in range(Tries):
             seed = randint(0, 100000) 
             execute(f"Simulation BH {N} {theta} {seed} BH_record.csv")
@@ -45,7 +45,7 @@ def gen_cmp_N_BH(theta):
         N_avg_list.append(average(N_avg))
         BH_avg_list.append(average(BH_avg))
 
-    # calculate average fps
+    # Enregistre les données dans un fichier csv pour les afficher plus tard
     with open(f"record_theta_{theta}.csv", "w+") as output:
         for i in range(len(N_list)):
             output.write(f"{int(N_list[i])}, {N_avg_list[i]}, {BH_avg_list[i]}\n")
@@ -55,6 +55,7 @@ def display_cmp(theta):
     N_avg_list = []
     BH_avg_list = []
 
+    # Récupère les données du fichier csv
     with open(f"record_theta_{theta}.csv", "r+") as file:
         rec = list(csv.reader(file))
         print(rec)
@@ -62,23 +63,19 @@ def display_cmp(theta):
             N_list.append(int(c[0]))
             N_avg_list.append(float(c[1]))
             BH_avg_list.append(float(c[2]))
-    
-    print(N_avg_list)
-    print(BH_avg_list)
 
+    # Affiche les données
     plt.plot(N_list, N_avg_list, label="Algorithme naif")
     plt.plot(N_list, BH_avg_list, label=f"Barnes-Hut ($theta$ = {theta})")
-
     plt.legend()
     plt.xlabel("N(Nombre d'etoile)")
     plt.ylabel(f"Temps moyen de calcule d'une image sur 100 images (s)")
-
     plt.show()
 
 def gen_naive():
     N_list = np.logspace(2, 4, num = 10, base=10.0)
     N_avg_list  = []
-    # Generate data
+    # Génère des données à théta constant et N variable pour l'algorithme naïf
     for N in N_list:
         print(f"{N=}")
         N_avg = []
@@ -89,6 +86,7 @@ def gen_naive():
             N_avg.append(average(to_float_list(N_record_list)))
         N_avg_list.append(average(N_avg))
 
+    # Enregistre les données dans un fichier csv pour les afficher plus tard
     with open(f"record_naive.csv", "w+") as output:
         for i in range(len(N_list)):
             output.write(f"{int(N_list[i])}, {N_avg_list[i]}\n")
@@ -103,63 +101,26 @@ def display_naive():
             N_list.append(int(c[0]))
             N_avg_list.append(float(c[1]))
     
+    # Affiche les données
     plt.plot(N_list,N_avg_list, "-x")
-
     plt.xlabel("N(Nombre d'etoile)")
     plt.ylabel(f"Temps moyen de calcule d'une image sur 100 images (s)")
-    plt.grid(True)
-    plt.show()
-
-
-def gen_BH_theta():
-    t_list = np.linspace(0.1, 1.5, 10)
-    print(t_list)
-    data_list  = []
-    # Generate data
-    for theta in t_list:
-        print(f"{theta=}")
-        t_avg = []
-        seed = randint(0, 100000) 
-        execute(f"Simulation BH 1000 {theta} {seed} t_record.csv")
-        with open("t_record.csv", "r+") as t_record:
-            t_record_list = np.array(list(csv.reader(t_record)))[:,1]
-            data_list.append(average(to_float_list(t_record_list)))
-
-    with open(f"record_t.csv", "w+") as output:
-        for i in range(len(t_list)):
-            output.write(f"{t_list[i]}, {data_list[i]}\n")
-
-def display_BH_t():
-    t_list = []
-    t_avg_list = []
-    with open(f"record_t.csv", "r+") as file:
-        rec = list(csv.reader(file))
-        for c in rec:
-            t_list.append(float(c[0]))
-            t_avg_list.append(float(c[1]))
-    lt = np.log(t_list)
-    lta = np.log(t_avg_list)
-    plt.plot(lt, lta)
-    a, b = np.polyfit(lt, lta, 1)
-    plt.title(f"a={float(a)}")
-    plt.xlabel("log(theta)")
-    plt.ylabel(f"Temps moyen de calcule d'une image sur 100 images(N=1000) (s)")
     plt.grid(True)
     plt.show()
 
 def gen_BH_N():
     N_list = np.logspace(2, 5, 15)
     data_list  = []
-    # Generate data
+    # Génère des données à théta constant et N variable pour l'algorithme de Barnes-Hut
     for N in N_list:
         print(f"{N=}")
-        t_avg = []
         seed = randint(0, 100000) 
         execute(f"Simulation BH {N} 1.0 {seed} N_record.csv")
         with open("N_record.csv", "r+") as N_record:
             N_record_list = np.array(list(csv.reader(N_record)))[:,1]
             data_list.append(average(to_float_list(N_record_list)))
 
+    # Enregistre les données dans un fichier csv pour les afficher plus tard
     with open(f"record_t.csv", "w+") as output:
         for i in range(len(N_list)):
             output.write(f"{N_list[i]}, {data_list[i]}\n")
@@ -167,29 +128,19 @@ def gen_BH_N():
 def display_BH_N():
     t_list = []
     t_avg_list = []
+
+    # Récupère les données du fichier csv
     with open(f"record_t.csv", "r+") as file:
         rec = list(csv.reader(file))
         for c in rec:
             t_list.append(float(c[0]))
             t_avg_list.append(float(c[1]))
+    
+    # Affiche les données
     plt.xlabel("N")
-    plt.plot(t_list, t_avg_list, label="T(N)/NlogN")
+    plt.plot(t_list, t_avg_list/(t_list), label="T(N)/N")
+    plt.plot(t_list, t_avg_list/(t_list)*np.log(t_list), label="T(N)/NlogN")
     plt.ylabel(f"Temps de calcul moyen sur 100 images (s)")
     plt.title("Theta = 1.0")
     plt.grid(True)
     plt.show() 
-
-    
-#for theta in [0.2, 0.5, 1]:
-#    gen_cmp_N_BH(theta)
-
-#display_data(1)
-
-#gen_naive()
-#display_naive()
-
-#gen_BH_theta()
-#display_BH_t()
-
-gen_BH_N()
-display_BH_N()
